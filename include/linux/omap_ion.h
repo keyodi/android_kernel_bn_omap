77758ac5e1fbe9021c874aaebf50d1a68aeb0bb3
D. Andrei Măceș <dmaces@nd.edu>
2015-12-01 13:49:04 -0500

    Revert "Revert "OMAP4: ION/TILER: Expose virtual stride and size""
    This reverts commit 38f3e721d89218dc7d9dcbc3e7160c6f354bd65f.
    
    Conflicts:
    	include/linux/omap_ion.h
    
    Change-Id: I9d5f362c78ced1653df5c671a73ac39daeb348b3

--- include/linux/omap_ion.h
+++ include/linux/omap_ion.h
@@ -70,6 +70,9 @@ int omap_tiler_pages(struct ion_client *client, struct ion_handle *handle,
 int omap_ion_share_fd_to_handles(int fd, struct ion_client *client,
 				struct ion_handle **handles, int *num_handles);
 struct ion_platform_heap * omap_ion_get2d_heap(void);
+int omap_tiler_vinfo(struct ion_client *client,
+			struct ion_handle *handle, unsigned int *vstride,
+			unsigned int *vsize);
 #endif /* __KERNEL__ */
 
 /* additional heaps used only on omap */
